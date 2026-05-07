f1dbc756cd7370aca3116af462cd40431ce1fc23
phngdg <125804675+Phngdg@users.noreply.github.com>
2026-05-07 10:39:32 +0700

    Fix command injection in file backup method
    Replaced system call with FileUtils.cp to prevent command injection vulnerabilities when backing up files.

--- app/models/benefits.rb
+++ app/models/benefits.rb
@@ -1,4 +1,7 @@
 # frozen_string_literal: true
+# Thêm thư viện xử lý file
+require 'fileutils'
+
 class Benefits < ApplicationRecord
 
   def self.save(file, backup = false)
@@ -12,7 +15,9 @@ class Benefits < ApplicationRecord
 
   def self.make_backup(file, data_path, full_file_name)
     if File.exist?(full_file_name)
-      silence_streams(STDERR) { system("cp #{full_file_name} #{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}") }
+      #silence_streams(STDERR) { system("cp #{full_file_name} #{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}") }
+      #Fix Command Injection: Sử dụng FileUtils.cp thay vì gọi system() để tránh Command Injection
+      FileUtils.cp(full_file_name, "#{data_path}/bak#{Time.zone.now.to_i}_#{file.original_filename}")
     end
   end
 
